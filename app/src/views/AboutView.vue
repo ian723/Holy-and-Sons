5407300729b4ecc4016d2cc44c4191be2b53aeab
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-09-28 02:43:30 +0300

    message

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-
+<Aboutus/>
     </div>
 </template>
 
 <script setup>
-
+import Aboutus from '../components/Aboutus.vue';
 </script>
 
 <style lang="scss" scoped>
--- app/src/views/ContactView.vue
+++ app/src/views/ContactView.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-
+<Contactus/>
     </div>
 </template>
 
 <script setup>
-
+import Contactus from '../components/Contactus.vue';
 </script>
 
 <style lang="scss" scoped>
