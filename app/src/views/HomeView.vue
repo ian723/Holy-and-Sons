cb5c670632a50dda9740b66c7054ab073beec3c3
Ian Ndegwa <ndegwaian241@gmail.com>
2024-09-28 20:09:16 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
 <Hero/>
+<Section/>
 <Homecards/>
+<Testimonials/>
 <Cards/>
 <Icon/>
   </div>
@@ -9,7 +11,9 @@
 
 <script setup>
 import Hero from '../components/Hero.vue';
+import Section from '../components/Section.vue';
 import Homecards from '../components/Homecards.vue';
+import Testimonials from '../components/Testimonials.vue';
 import Cards from '../components/Cards.vue';
 import Icon from '../components/Icon.vue';
 </script>
