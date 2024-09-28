a36328656a28cbd5139055027fd3a537ce8b2ef0
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-09-28 15:44:58 +0300

    message

--- app/src/components/Icon.vue
+++ app/src/components/Icon.vue
@@ -8,7 +8,7 @@
   <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
 </svg>
     </div>
-    <h2 class="text-lg font-semibold mb-2 mr-10">Where to buy</h2>
+    <h2 class="text-2xl font-semibold mb-2 mr-10">Where to buy</h2>
     <a href="#" class="text-blue-800 hover:underline">Learn More &gt;</a>
   </div>
 
@@ -20,7 +20,7 @@
   <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5"/>
 </svg>
     </div>
-    <h2 class="text-lg font-semibold mb-2 mr-8">Support</h2>
+    <h2 class="text-2xl font-semibold mb-2 mr-8">Support</h2>
     <a href="#" class="text-blue-800 hover:underline">Learn More &gt;</a>
   </div>
 
@@ -32,7 +32,7 @@
   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
 </svg>
     </div>
-    <h2 class="text-xl font-semibold mb-2 mr-8">Fly Safe</h2>
+    <h2 class="text-2xl font-semibold mb-2 mr-8">Fly Safe</h2>
     <a href="#" class="text-blue-800 hover:underline">Learn More &gt;</a>
   </div>
 </div>
