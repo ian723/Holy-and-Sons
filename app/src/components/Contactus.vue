2e8e304c79a0167ec3d81202c15a356929a6dd75
ian723 <ndegwaian241@gmail.com>
2024-10-01 11:57:58 +0300

    message

--- app/src/components/Contactus.vue
+++ app/src/components/Contactus.vue
@@ -3,16 +3,14 @@
     <section class="bg-blue-50 dark:bg-slate-800" id="contact">
       <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
         <div class="mb-4">
-          <div
-            class="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12"
-          >
+          <div class="mb-6 max-w-3xl text-center md:mx-auto md:mb-12">
             <p
               class="text-base font-semibold uppercase tracking-wide text-blue-600 dark:text-blue-200"
             >
               Contact
             </p>
             <h2
-              class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl"
+              class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-4xl md:text-5xl"
             >
               Get in Touch
             </h2>
@@ -24,22 +22,21 @@
           </div>
         </div>
 
-        <div class="flex items-stretch justify-center">
-          <div class="grid md:grid-cols-2">
+        <div class="flex flex-col md:flex-row items-stretch justify-center">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
             <!-- Left Section -->
-            <div class="h-full pr-6">
+            <div class="md:pr-6">
               <p class="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
                 Class aptent taciti sociosqu ad litora torquent per conubia
                 nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut
                 scelerisque sagittis ante, ac tincidunt sem venenatis ut.
               </p>
               <!-- Address, Contact, Working Hours -->
-              <ul class="mb-6 md:mb-0">
+              <ul class="space-y-6">
                 <li class="flex">
                   <div
                     class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50"
                   >
-                    <!-- Icon -->
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -58,17 +55,14 @@
                       ></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
+                  <div class="ml-4">
                     <h3
                       class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white"
                     >
                       Our Address
                     </h3>
                     <p class="text-gray-600 dark:text-slate-400">
-                      Light house building along kenyatta Avenue
-                    </p>
-                    <p class="text-gray-600 dark:text-slate-400">
-                      Nakuru, Kenya
+                      Light house building along Kenyatta Avenue, Nakuru, Kenya
                     </p>
                   </div>
                 </li>
@@ -95,7 +89,7 @@
                       <path d="M15 3a6 6 0 0 1 6 6"></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
+                  <div class="ml-4">
                     <h3
                       class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white"
                     >
@@ -129,17 +123,17 @@
                       <path d="M12 7v5l3 3"></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
+                  <div class="ml-4">
                     <h3
                       class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white"
                     >
                       Working hours
                     </h3>
                     <p class="text-gray-600 dark:text-slate-400">
-                      Monday - Friday: 08:00 - 17:00
+                      Mon - Fri: 08:00 - 17:00
                     </p>
                     <p class="text-gray-600 dark:text-slate-400">
-                      Saturday & Sunday: 08:00 - 12:00
+                      Sat & Sun: 08:00 - 12:00
                     </p>
                   </div>
                 </li>
@@ -152,43 +146,38 @@
                 Ready to Get Started?
               </h2>
               <form id="contactForm">
-                <div class="mb-6">
-                  <div class="mx-0 mb-1 sm:mb-4">
-                    <label for="name"></label>
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-6">
+                  <div class="col-span-2 sm:col-span-1">
                     <input
                       type="text"
                       id="name"
                       placeholder="Your name"
-                      class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
-                      name="name"
+                      class="w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300"
                     />
                   </div>
-                  <div class="mx-0 mb-1 sm:mb-4">
-                    <label for="email"></label>
+                  <div class="col-span-2 sm:col-span-1">
                     <input
                       type="email"
                       id="email"
                       placeholder="Your email address"
-                      class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
-                      name="email"
+                      class="w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300"
                     />
                   </div>
-                  <div class="mx-0 mb-1 sm:mb-4">
-                    <label for="textarea"></label>
+                  <div class="col-span-2">
                     <textarea
                       id="textarea"
                       name="textarea"
                       cols="30"
                       rows="5"
                       placeholder="Write your message..."
-                      class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                      class="w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300"
                     ></textarea>
                   </div>
                 </div>
                 <div class="text-center">
                   <button
                     type="submit"
-                    class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0"
+                    class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md"
                   >
                     Send Message
                   </button>
@@ -203,12 +192,13 @@
                 <div class="w-full h-80">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d249.3605577513303!2d36.06913381648718!3d-0.28510761959188413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x18298d95bffee3eb%3A0xf9f5bab9efd1dad4!2sKenyatta%20Ave%2C%20Nakuru!5e0!3m2!1sen!2ske!4v1727692440373!5m2!1sen!2ske"
-                    width="600"
-                    height="450"
+                    class="w-full h-full"
                     style="border: 0"
+                    allowfullscreen=""
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"
-                  ></iframe>
+                  >
+                  </iframe>
                 </div>
               </div>
             </div>
